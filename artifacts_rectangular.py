20a0c0ae37fa889287ea7da44d830347984784d6
Jayasurya <smjayasurya1997@gmail.com>
2020-08-25 21:10:18 -0700

    Initial Commit

--- artifacts_rectangular.py
+++ artifacts_rectangular.py
@@ -1,7 +1,7 @@
-
+from copy import deepcopy
 alphabets="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 numbers="0123456789"
-artifacts="1A 1B,2C 2D"
+artifacts="1A 1B,2C 2C"
 searched="1B"
 N=3
 temp_artifact_list=list(map(str,artifacts.split(",")))
@@ -25,6 +25,7 @@ for i in range (0,len(artifact_list)-1,2):
     top_column=alphabets.index(artifact_list[i][-1])
     bottom_row=int(artifact_list[i+1][:-1])-1
     bottom_column=alphabets.index(artifact_list[i+1][-1])
+    print(top_row,top_column,bottom_row,bottom_column)
     if(top_row==bottom_row):
         for i in range(0,bottom_column-top_column+1):
             area[top_row][top_column+i]=count
@@ -39,23 +40,25 @@ for i in range (0,len(artifact_list)-1,2):
         area[bottom_row][bottom_column]=count
     count=count+1
 print(area)        
-
+area_copy=deepcopy(area)
 for i in searched_list:
     row=int(i[0])
     column=alphabets.index(i[1])
     #print(row,column)
-    area[row-1][column]="X"
-print(area)
+    area_copy[row-1][column]="X"
+print(area_copy)
+#print(area)
 area = [j for sub in area for j in sub]
+area_copy = [j for sub in area_copy for j in sub]
 completed=0
 partial=0
 for i in range(count-1,0,-1):
     #print(i)
-    #print(i,area.count(i))
-    if (area.count(i)==0):
+    #print("area count",i,area.count(str(i)))
+    #print("area copy count",i,area_copy.count(str(i)))
+    if (area_copy.count(i)==0):
         completed+=1
-    else:
-        partial+=1
+    elif(area_copy.count(i)!=area.count(i)):
+      partial+=1
     
 print(completed, partial)
-    
