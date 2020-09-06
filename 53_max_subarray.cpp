dfac83c6454ab9b847aa9e00e67aa4c072eedba1
Jayasurya <smjayasurya1997@gmail.com>
2020-09-05 23:50:56 -0700

    Kadane's Algorithm

--- 53_max_subarray.cpp
+++ 53_max_subarray.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int maxSub = nums[0], curSum = 0;
+        int maxSub = nums[0];
+        int curSum = 0;
         
         for (int n : nums) {
             if (curSum < 0)
@@ -9,6 +10,8 @@ public:
             curSum += n;
             maxSub = max(maxSub, curSum);
         }
+
+
         return maxSub;
     }
 };
