eb4cac09f9f426df1409ae178be828aa3c2abda9
Jayasurya <smjayasurya1997@gmail.com>
2020-12-14 22:47:53 +0530

    heapify solution klog(n)

---
+++ 692_top_k_frequent.py
@@ -0,0 +1,13 @@
+import heapq
+
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        heap = [] # Space O(N)
+        counter=collections.Counter(words)
+        for word, count in counter.items(): # M: num of words w/o duplicate Time O(M*logM)
+            heapq.heappush(heap, (-count, word))
+        res = []
+        for i in range(k): # Time O(k*logM)
+            count, word = heapq.heappop(heap)
+            res.append(word)
+        return res
