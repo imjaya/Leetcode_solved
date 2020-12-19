28de3c9a4c42eca89cc3104b6650ee575b382dad
Jayasurya <smjayasurya1997@gmail.com>
2020-12-19 23:57:15 +0530

    two sigma question 1 missing word

--- twosigma_missing_words.py
+++ twosigma_missing_words.py
@@ -1,3 +1,4 @@
+#make a dict of bigger string and then for every word in the second string remove the count in the dict, return the list of remaining words
 def missingWords(s, t):
     ls = s.split(' ')
     lt = t.split(' ')
