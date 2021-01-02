580b71e41a08bc1af464985d0697f229da9865ff
Jayasurya <smjayasurya1997@gmail.com>
2021-01-03 01:05:43 +0530

    two pointer approach

---
+++ 443_StringCompression.py
@@ -0,0 +1,25 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        length = 0
+        count = 1
+        chars.append("\n") # For last char
+        
+        for i in range(1, len(chars)):
+            if chars[i] == chars[i - 1]:
+                count += 1
+                continue
+            
+            # Write char
+            chars[length] = chars[i - 1]
+            length += 1
+            
+            if count == 1: continue
+            # Write digis
+            for n in str(count):
+                chars[length] = n
+                length += 1
+            
+            # Reset counter
+            count = 1
+            
+        return length
