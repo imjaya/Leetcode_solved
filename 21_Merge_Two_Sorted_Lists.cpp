7c5ce2925214447ba0ec706b45eb1e08f7d54c8c
Jayasurya <smjayasurya1997@gmail.com>
2021-01-21 02:17:30 +0530

    simple recursion approach

---
+++ 21_Merge_Two_Sorted_Lists.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(l1 == NULL) return l2;
+        if(l2 == NULL) return l1;
+        
+        if(l1->val > l2->val) return mergeTwoLists(l2, l1);
+        
+        ListNode* temp = l1->next;
+        l1->next = mergeTwoLists(temp, l2);
+        
+        return l1;
+    }
