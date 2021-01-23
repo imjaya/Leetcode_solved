8bfb0dd1feb68b71e964b57e2fe2bca3c13ce73d
Jayasurya <smjayasurya1997@gmail.com>
2021-01-23 10:59:44 +0530

    recursive approach

---
+++ 21_Merge_Two_Sorted_Lists.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        
+        head = ListNode()
+        start = head
+        
+        while l1 or l2:
+            if l1 and l2:
+                if l1.val<=l2.val:
+                    head.next = l1
+                    l1 = l1.next
+                else:
+                    head.next = l2
+                    l2=l2.next
+            elif l1 and not l2:
+                head.next = l1
+                break
+            else:
+                head.next = l2
+                break
+            
+            head = head.next
+        
+        return start.next
+        
