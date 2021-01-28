86c3972ac8c8aeba5cb75718e2207bb5ae8694d3
Jayasurya <smjayasurya1997@gmail.com>
2021-01-27 23:27:18 -0700

    Dynamic Programming

---
+++ 518_Coin_Change_2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+	int change(int amount, vector<int>& coins) {
+
+		int dp[amount+1];
+		memset(dp, 0, sizeof dp);
+
+		dp[0] = 1;
+
+		for(auto coin: coins){
+			for(int i = coin ; i <= amount ; i++){
+				dp[i] += dp[i-coin];
+			}
+		}
+
+		return dp[amount];
+	}
+};
