256845e6ef19d2803c5708eeaf2865215b809111
Jayasurya <smjayasurya1997@gmail.com>
2021-02-18 21:10:40 -0700

    set based approach

---
+++ 287_Find_the_Duplicate_Number.cpp
@@ -0,0 +1,14 @@
+//Using Set Concept
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        unordered_set<int> set;
+        for(int i=0; i<nums.size();i++)
+        {
+            if(set.count(nums[i])>0)
+                return nums[i];
+            set.insert(nums[i]);
+        }
+        return -1;
+    }
+};
