fe80d41c652d33b72e5b2d6c7177ef540737c824
Jayasurya <smjayasurya1997@gmail.com>
2021-05-04 10:45:15 -0700

    Merge Sort

---
+++ 315_Count_of_Smaller_Numbers_After_Self.py
@@ -0,0 +1,40 @@
+class Solution:
+    def countSmaller(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        arr = [[v, i] for i, v in enumerate(nums)]  
+        result = [0] * n
+
+        def merge_sort(arr, left, right):
+            if right - left <= 1:
+                return
+            mid = (left + right) // 2
+            merge_sort(arr, left, mid)
+            merge_sort(arr, mid, right)
+            merge(arr, left, right, mid)
+
+        def merge(arr, left, right, mid):
+            # merge [left, mid) and [mid, right)
+            i = left 
+            j = mid
+            temp = []
+            while i < mid and j < right:
+                if arr[i][0] <= arr[j][0]:
+                    result[arr[i][1]] += j - mid
+                    temp.append(arr[i])
+                    i += 1
+                else:
+                    temp.append(arr[j])
+                    j += 1
+            while i < mid:
+                result[arr[i][1]] += j - mid
+                temp.append(arr[i])
+                i += 1
+            while j < right:
+                temp.append(arr[j])
+                j += 1
+            for i in range(left, right):
+                arr[i] = temp[i - left]
+
+        merge_sort(arr, 0, n)
+
+        return result
