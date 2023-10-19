e09e17f84efa1436b534df3ad196d69522318a73
imjaya <smjayasurya1997@gmail.com>
2023-10-18 20:54:24 -0700

    Longest substring without repeating chars
    Using sliding window technique.

--- 3_longest_Substring_without_Repeating_character.py
+++ 3_longest_Substring_without_Repeating_character.py
@@ -11,4 +11,27 @@ class Solution:
                 
         maxx = len(result) if len(result) > maxx else maxx
         return maxx
-      #what approach       
+      #what approach
+
+#Two pointer sliding window approach TC: O(N) SC: O(N)
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        n= len(s)
+        left, right = 0, 0
+        max_length = 0
+        visited_chars = set()
+        if n <= 1:
+            return n
+
+        while left < n:
+            if right == n or s[right] in visited_chars:
+                max_length = max(max_length, right - left)
+                left += 1
+                right = left
+                visited_chars = set()
+                
+            else:
+                visited_chars.add(s[right])
+                right += 1
+                
+        return max(max_length, right - left )   
