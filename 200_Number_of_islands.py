7b5dc8fca04ef7569baa3dc51343821c195f4a09
imjaya <smjayasurya1997@gmail.com>
2023-10-18 22:55:51 -0700

    Number of Islands
    BFS and DFS.

--- 200_Number_of_islands.py
+++ 200_Number_of_islands.py
@@ -1,40 +1,50 @@
+# DFS TC: O(M*N) SC: O(M*N)
 class Solution:
-    
-    
-    
     def numIslands(self, grid: List[List[str]]) -> int:
+        neighbours = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        def dfs(grid, i, j):
+            grid[i][j] = "0"
+            for neighbour in neighbours:
+                x = i + neighbour[0]
+                y = j + neighbour[1]
+                if x >= 0 and x < len(grid) and y >=0 and y < len(grid[0]) and grid[x][y] =="1":
+                    dfs(grid, x , y) 
 
-        island_count = 0
+        num_islands = 0
+        for i in range(0, len(grid)):
+            for j in range(0,len((grid[0]))):
+                if grid[i][j] == "1":
+                    num_islands += 1
+                    dfs(grid, i, j)
 
-        for i in range(0,len(grid)):
-            for j in range(0,len(grid[i])):
-                if grid[i][j] == '1':
-                    self.dfs(i, j, grid)
-                    island_count += 1
-
-        return island_count
-
-
-    def dfs(self, i, j, grid):
-
-        if i < 0 or i >= len(grid):
-            return
-
-        if j < 0 or j >= len(grid[0]):
-            return
-
-        if grid[i][j] == '0':
-            return
-
-        grid[i][j] = '0'
-
-        self.dfs(i - 1, j, grid)
-        self.dfs(i + 1, j, grid)
-        self.dfs(i, j - 1, grid)
-        self.dfs(i, j + 1, grid)
-
-
+        return num_islands
     
+# BFS TC: O(M*N) SC: O(min(M, N))
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        neighbours = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        count = 0
+
+        def bfs(grid, q):
+            while q:
+                i, j = q.popleft()
+                for neighbour in neighbours:
+                    x = i + neighbour[0]
+                    y = j + neighbour[1]
+                    if x >= 0 and x < len(grid) and y >=0 and y < len(grid[0]) and grid[x][y] =="1":
+                        grid[x][y] = "0"
+                        q.append((x, y))
+
+        q = deque()
+        num_islands = 0
+        for i in range(0, len(grid)):
+            for j in range(0,len((grid[0]))):
+                if grid[i][j] == "1":
+                    grid[i][j] = "0"
+                    q.append((i, j))
+                    bfs(grid, q)
+                    num_islands += 1
+
+        return num_islands
         
-                    
         
